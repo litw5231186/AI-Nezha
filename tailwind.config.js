2f6d35fe9f5c0011f2b4588a5bafcb608beacdd6
litw5231186 <litw3@lenovo.com>
2025-05-16 09:33:39 +0800

    Add files via upload
    Signed-off-by: litw5231186 <litw3@lenovo.com>

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000', // 黑色作为主色
+        secondary: '#FFFFFF', // 白色作为辅色
+        accent: '#FF0000', // 红色作为强调色
+      },
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'], // 无衬线字体
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+    },
+  },
+  plugins: [],
+}
